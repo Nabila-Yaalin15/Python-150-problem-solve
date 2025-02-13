70d0590b8f0adef823cc4cf2cc645cc1954ba16b
Nabila Rahman Setu <nabila153335setu@gmail.com>
2025-02-13 22:22:10 +0600

    Nabila

---
+++ Problem-1.py
@@ -0,0 +1,7 @@
+#1. Variable Swap: Write a Python program to swap the values of two variables without using a temporary variable.
+
+a=4
+b=6
+print("Before swapping: a =",a,",b=",b)
+a,b=b,a
+print("After swapping: a =",a,",b=",b)
---
+++ problem-2.py
@@ -0,0 +1,8 @@
+#2. Even or Odd: Write a Python program that takes an integer as input and prints whether it is even or odd.
+
+number =int(input("Enter a number: "))
+
+if number %2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
